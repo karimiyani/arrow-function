10719550c91625d004943ff4a356c0124e887008
karimiyani <front_end2020@yahoo.com>
2021-09-27 09:32:10 +0330

    first commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+function add ( a, b){
+    return a+b;
+}
+add(3, 4);
+
+//ravesh 1
+//first delete function keyboard
+//second define variable ,choose the same name of function
+//third assign function to the variable 
+
+const add = function (a, b) {
+    return (a+ b);
+};
+
+
+
+const add = (a, b) =>  a+ b;
+
+// const add = a => a+ 1;
